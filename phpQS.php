efa1d17234349a89718b392579d1d943e9762a36
haniramadhan <hani.its.042@gmail.com>
2019-07-01 21:45:30 +0900

    deleted analyze feature

--- phpQS.php
+++ phpQS.php
@@ -149,42 +149,6 @@ if(isset($_POST['submit'])) {
         
         echo "EHLLP;"
 
-        $request = new Http_Request2($uriBase . '/analyze');
-        $url = $request->getUrl();
-
-        $headers = array(
-            // Request headers
-            'Content-Type' => 'application/json',
-            'Ocp-Apim-Subscription-Key' => $subscriptionKey
-        );
-        $request->setHeader($headers);
-
-        $parameters = array(
-            // Request parameters
-            'visualFeatures' => 'Categories,Description',
-            'details' => '',
-            'language' => 'en'
-        );
-        $url->setQueryVariables($parameters);
-
-        $request->setMethod(HTTP_Request2::METHOD_POST);
-
-        // Request body parameters
-        $body = json_encode(array('url' => $imageUrl));
-
-        // Request body
-        $request->setBody($body);
-
-        try
-        {
-            $response = $request->send();
-            echo "<pre>" .
-                json_encode(json_decode($response->getBody()), JSON_PRETTY_PRINT) . "</pre>";
-        }
-        catch (HttpException $ex)
-        {
-            echo "<pre>" . $ex . "</pre>";
-        }
     }
 
 }
