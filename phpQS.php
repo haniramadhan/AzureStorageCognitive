ec42567255b9625e02d2402a298d3e59e7d77f0f
haniramadhan <hani.its.042@gmail.com>
2019-07-02 00:59:09 +0900

    edit last

--- phpQS.php
+++ phpQS.php
@@ -181,9 +181,10 @@ if(isset($_POST['submit'])) {
         {
             $response = $request->send();
             $json = $response->getBody();
-            echo "<img src=".$imageUrl."/>";
+            echo "<img src=\"".$imageUrl."\"/>";
             echo "TEST";
             echo $json["description"]["captions"][0]["text"];
+            echo "<pre>" . json_encode(json_decode($json)), JSON_PRETTY_PRINT) . "</pre>";
 
         }
         catch (HttpException $ex)
